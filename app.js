3716525f7a4091334e25f01588890755e126a17c
Nicole <nicoleshek315@gmail.com>
2020-08-08 17:08:15 +0200

    Added sliding text to display each skill

--- app.js
+++ app.js
@@ -7,28 +7,31 @@ function typingEffect() {
     const typeText = document.querySelector('.typing');
     const typeElement = document.querySelector('.typing-text');
     const colourClass = ['my-name-text', 'developer-text', 'ramen-text'];
-
+    let prevColour;
 
     (function type() {
-        //add appropriate colour class
-        //remove previous colour class
-        typeText.classList.add(colourClass[count]);
-        typeText.classList.remove(colourClass[count - 1]);
-
         if (count === texts.length) {
             count = 0;
         }
+
+        //add appropriate colour class
+        //remove previous colour class
+        typeText.classList.remove(prevColour);
+        typeText.classList.add(colourClass[count]);
+        // typeText.classList.remove(colourClass[count === 0 ? colourClass.length - 1 : count - 1]);
+        prevColour = colourClass[count];
+
         currentText = texts[count];
         // add colour
         letter = currentText.slice(0, ++index);
-        t = setTimeout(type, 300);
+
 
         typeText.textContent = letter;
         if (letter.length === currentText.length) {
             count++;
             index = 0;
         };
-
+        t = setTimeout(type, 330);
 
 
         //pause typing animation if mouse is hovering over
@@ -38,14 +41,87 @@ function typingEffect() {
 
         //restart typing animation if mouse is no longer hovering over
         typeElement.onmouseleave = function () {
-            setTimeout(type, 300);
+            setTimeout(type, 330);
         };
 
     })();
 };
 
-function main() {
-    typingEffect();
+
+// Nav bar underline sections animation
+function navAnimation() {
+    const sections = document.querySelectorAll("section");
+    const navLinks = document.querySelectorAll("nav a");
+
+    console.log(navLinks);
+    sections.forEach(section => {
+        section.addEventListener("mouseenter", function () {
+            const id = this.getAttribute("id");
+            const navActive = document.querySelector(`a[href="#${id}"]`);
+            navLinks.forEach(link => link.classList.remove('active'));
+            navActive.classList.add('active');
+
+        });
+    });
 };
 
-// main();
+
+//smooth scrolling effect
+function scrollEffect() {
+    const links = document.querySelectorAll("nav a");
+
+    for (const link of links) {
+        link.addEventListener("click", clickHandler);
+    }
+
+    function clickHandler(e) {
+        e.preventDefault();
+        const href = this.getAttribute("href");
+        const offsetTop = document.querySelector(href).offsetTop;
+
+        scroll({
+            top: offsetTop,
+            behavior: "smooth"
+        });
+    }
+}
+
+// Using Intersection Observer for Navbar test
+function observeSectionForNav() {
+    const sections = document.querySelectorAll('section');
+    const options = {
+        // root: null, //this is the viewport
+        threshold: 0.4
+        // rootMargin: "-150px"
+    };
+
+    const navObserver = new IntersectionObserver(function (entries, observer) {
+        entries.forEach(entry => {
+            const idName = entry.target.id;
+            const activeAnchor = document.querySelector(`[data-page=${idName}]`);
+            if(!entry.isIntersecting){
+                activeAnchor.classList.remove('active');
+            } else{
+                activeAnchor.classList.add('active');
+            }
+
+        });
+    }, options);
+
+    sections.forEach(section => {
+        navObserver.observe(section);
+        console.log(section);
+    })
+}
+
+
+
+//main function
+function main() {
+    scrollEffect();
+    // typingEffect();
+    // navAnimation();
+    observeSectionForNav();
+};
+
+main();
